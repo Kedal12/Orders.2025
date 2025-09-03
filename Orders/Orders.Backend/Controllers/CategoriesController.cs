ddcc103e8521c3996d8740cc99b35c8530863891
KEVIN DAVID LONDONO ROQUEME <kevinlondono1122890@correo.itm.edu.co>
2025-09-03 11:04:35 -0500

    Complete generic controller

---
+++ Orders/Orders.Backend/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController : GenericControllerz<Category>
+{
+    public CategoriesController(UnitsOfWork.Interfaces.IGenericUnitOfWork<Category> unitOfWork) : base(unitOfWork)
+    {
+    }
+}
